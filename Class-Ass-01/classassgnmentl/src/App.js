10f48b4f53cfcdc5833390c30e69dfda7a6cc467
itsShubhamShinde <itsshubhamks@gmail.com>
2023-04-26 22:43:35 +0530

    first commit

---
+++ Class-Ass-01/classassgnmentl/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Learn web development</h1>
+<br />
+      <p>Welcome to the MDN learning area. This set of articles aims to guide complete </p>
+      <p>beginners to web development with all that they need to start coding websites</p>
+      <br />
+      <p>The aim of this area of MDN is not to take you from "beginner" to "expert" but to</p>
+      <p>take you from "beginner" to "comfortable." From there, you should be able to start</p>
+      <p>making your way, learning from <a href="">the rest of MDN</a>, and other intermediate to</p>
+      <p>advanced resources that assume a lot of previous knowledge.</p>
+      <br />
+      <p>If you are a complete beginner, web development can be challenging - we will hold</p>
+      <p>your hand and provide enough detail for you to feel comfortable and learn the topics</p>
+      <p>properly. You should feel at home whether you are a student learning web</p>
+      <p>development (on your own or as part of a class), a teacher looking for class</p>
+      <p>materials, a hobbyist, or someone who just wants to understand more about how</p>
+      <p>web technologies work.</p>
+    </div>
+  );
+}
+
+export default App;
